d39cae85e8b938b7460ad6d9dc86be605adf9017
Joseph cohen <jojocohen57@gmail.com>
2020-08-18 18:07:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,9 +6,9 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/piglatinizer' do
+  post '/piglatinize' do
     pl = PigLatinizer.new
-    @piglatin = pl.piglatinizer(params[:user_phrase])
+    @piglatin = pl.piglatinize(params[:user_phrase])
     erb :results
   end
 
